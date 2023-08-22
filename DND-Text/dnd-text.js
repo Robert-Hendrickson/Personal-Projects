77a02700eaf8613a13b3e8ccf351bf7a96d1e921
Robert-Hendrickson <robert.hendrickson15@gmail.com>
2023-08-21 19:10:47 -0600

    updated script to update example text to the selected font as well as update the translated text

--- DND-Text/dnd-text.js
+++ DND-Text/dnd-text.js
@@ -1,3 +1,10 @@
 function textUpdate(){
     $('textarea#translated-text')[0].value = $('textarea#entered-text')[0].value;
 }
+function fontSelect(){
+    let x = $('.font-selector select')[0].value;
+    let example = $('#example-text')[0];
+    let content = $('textarea#translated-text')[0];
+    example.style.fontFamily = x;
+    content.style.fontFamily = x;
+}
