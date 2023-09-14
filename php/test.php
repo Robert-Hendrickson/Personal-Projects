c2cb773585bf4ff10f69a5acc744dffb1bb19a61
Robert <robert.hendrickson15@gmail.com>
2023-09-14 15:26:48 -0600

    test php file for github

--- php/test.php
+++ php/test.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
+<?php
+$passed_info = false;
+if($_GET['el']){
+    $type = $_GET['el'];
+    $passed_info = true;
+}
+?>
 <html>
     <body>
-        <?php echo "test text";?>
+        <?php if(!$passed_info){?>
+            "test text";
+        <?php }elseif($passed_info){?>
+            "thanks for the info"
+            <?php }; ?>
     </body>
 </html>
